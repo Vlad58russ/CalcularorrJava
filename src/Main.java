930e29938158cc7a78d9624cc6fda350b7e2ffc7
Vlad <vladschenn@yandex.ru>
2023-05-08 12:06:19 +0300

    vvv

--- src/Main.java
+++ src/Main.java
@@ -10,7 +10,6 @@ public class Main {
     }
 
     public static String calc(String input) throws Exception {
-        Converter converter = new Converter();
         String[] actionSign = new String[]{"+", "-", "/", "*"};
         String[] regexActions = {"\\+", "-", "/", "\\*"};
         int actionIndex = -1;
@@ -29,54 +28,36 @@ public class Main {
         if (arrStr.length != 2) {
             throw new Exception("Вы ввели не 2 числа");
         }
-        if (converter.isRoman(arrStr[0]) == converter.isRoman(arrStr[1])) {
-            int num1;
-            int num2;
-            boolean isRoman = converter.isRoman(arrStr[0]);
-            if (isRoman) {
 
-                num1 = converter.romanToArab(arrStr[0]);
-                num2 = converter.romanToArab(arrStr[1]);
+        int num1;
+        int num2;
 
-            } else {
+        num1 = Integer.parseInt(arrStr[0]);
+        num2 = Integer.parseInt(arrStr[1]);
 
-                num1 = Integer.parseInt(arrStr[0]);
-                num2 = Integer.parseInt(arrStr[1]);
-            }
-            if (num1 > 10 || num2 > 10) {
-                throw new Exception("Числа должны быть до 10");
-            }
-
-            int result = 0;
-            switch (actionSign[actionIndex]) {
-                case "+":
-                    result = num1 + num2;
-                    break;
-                case "-":
-                    result = num1 - num2;
-                    break;
-                case "*":
-                    result = num1 * num2;
-                    break;
-                case "/":
-                    if (num2 == 0) {
-                        throw new Exception("На ноль делить нельзя");
-                    }
-                    result = num1 / num2;
-                    break;
-            }
-            if (isRoman) {
-                if (result < 1) {
-                    throw new Exception("Римские числа не могут быть отрицательными или '0' ");
-                }
-                System.out.println("Output:\n" + converter.arabToRoman(result));
-            } else {
-                System.out.println("Output:\n" + result);
-            }
-
-        } else {
-            throw new Exception("Числа должны быть в одном формате");
+        if (num1 > 10 || num2 > 10) {
+            throw new Exception("Числа должны быть до 10");
         }
+
+        int result = 0;
+        switch (actionSign[actionIndex]) {
+            case "+":
+                result = num1 + num2;
+                break;
+            case "-":
+                result = num1 - num2;
+                break;
+            case "*":
+                result = num1 * num2;
+                break;
+            case "/":
+                if (num2 == 0) {
+                    throw new Exception("На ноль делить нельзя");
+                }
+                result = num1 / num2;
+                break;
+        }
+        System.out.println("Output:\n" + result);
         return input;
     }
 }
